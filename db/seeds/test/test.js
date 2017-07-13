c7c9f1db19ffd6c538f7ccc555b38396aa062d47
Anders Wood <anderswood@gmail.com>
2017-07-13 00:19:47 -0600

    add 8 out of 10 end points and associated testing

--- db/seeds/test/test.js
+++ db/seeds/test/test.js
@@ -14,122 +14,136 @@ const fuelCodeLegend = {
 }
 
 const E85Station = {
+  id: 1,
   station_code: 41733,
   station_name: "Veterans Affairs - Maine Healthcare System - Togus",
-  zip: 04330,
+  zip: '04330',
   state: 'ME',
   city: "Augusta",
   street_address: "1 VA Center",
-  latitude: 44.296869,
-  longitude: -69.724097,
+  latitude: '44.296869',
+  longitude: '-69.724097',
   geocode_status: "GPS"
 };
 const ElecStation1 = {
+  id: 2,
   station_code: 44280,
   station_name: "Lee Nissan of Auburn",
-  zip: 04210,
+  zip: '04210',
   state: 'ME',
   city: "Auburn",
   street_address: "793 Center St",
-  latitude: 44.129233,
-  longitude: -70.22363,
+  latitude: '44.129233',
+  longitude: '-70.22363',
   geocode_status: "200-8"
 };
 const ElecStation2 = {
+  id: 3,
   station_code: 48404,
   station_name: "ReVision Energy",
-  zip: 04103,
+  zip: '04103',
   state: 'ME',
   city: "Portland",
   street_address: "142 Presumpscot St",
-  latitude: 43.686832,
-  longitude: -70.260117,
+  latitude: '43.686832',
+  longitude: '-70.260117',
   geocode_status: "GPS"
 };
 const BDStation = {
+  id: 4,
   station_code: 48463,
   station_name: "Acadia National Park - Bar Harbor Fuel System",
-  zip: 04609,
+  zip: '04609',
   state: 'ME',
   city: "Bar Harbor",
   street_address: "Rt 233 McFarland Hill",
-  latitude: 44.398072,
-  longitude: -68.283333,
+  latitude: '44.398072',
+  longitude: '-68.283333',
   geocode_status: "GPS"
 };
 const CNGStation = {
+  id: 5,
   station_code: 61679,
   station_name: "Bangor Natural Gas ",
-  zip: 04401,
+  zip: '04401',
   state: 'ME',
   city: "Bangor",
   street_address: "498 Maine Ave",
-  latitude: 44.8094139,
-  longitude: -68.8046418,
+  latitude: '44.8094139',
+  longitude: '-68.8046418',
   geocode_status: "200-8"
 };
 const LPGStation1 = {
+  id: 6,
   station_code: 62740,
   station_name: "U-Haul",
-  zip: 04101,
+  zip: '04101',
   state: 'ME',
   city: "Portland",
   street_address: "411 Marginal Way",
-  latitude: 43.670522,
-  longitude: -70.257538,
+  latitude: '43.670522',
+  longitude: '-70.257538',
   geocode_status: "200-8"
 };
 const LPGStation2 = {
+  id: 7,
   station_code: 63965,
   station_name: "Bob's Cash Fuel Inc",
-  zip: 04950,
+  zip: '04950',
   state: 'ME',
   city: "Madison",
   street_address: "424 Main St",
-  latitude: 44.800429,
-  longitude: -69.853698,
+  latitude: '44.800429',
+  longitude: '-69.853698',
   geocode_status: "GPS"
 };
 
 const processStationData = Data => {
   let fuelDataProcessed = [
     {
+      id: 11,
       fuel_type_code: 'E85',
       fuel_type: 'Ethanol (E85)',
       count: 1,
       stations: [E85Station]
     },
     {
+      id: 12,
       fuel_type_code: 'ELEC',
       fuel_type: 'Electric',
       count: 2,
       stations: [ElecStation1, ElecStation2]
     },
     {
+      id: 13,
       fuel_type_code: 'HY',
       fuel_type: 'Hydrogen',
       count: 0,
       stations: []
     },
     {
+      id: 14,
       fuel_type_code: 'LNG',
       fuel_type: 'Liquefied Natural Gas',
       count: 0,
       stations: []
     },
     {
+      id: 15,
       fuel_type_code: 'BD',
       fuel_type: 'Biodiesel (B20 and above)',
       count: 1,
       stations: [BDStation]
     },
     {
+      id: 16,
       fuel_type_code: 'CNG',
       fuel_type: 'Compressed Natural Gas',
       count: 1,
       stations: [CNGStation]
     },
     {
+      id: 17,
       fuel_type_code: 'LPG',
       fuel_type: 'Liquefied Petroleum Gas (Propane)',
       count: 2,
@@ -141,7 +155,9 @@ const processStationData = Data => {
 };
 
 const createFuelType = (knex, fuel) => {
+
   return knex('fuel_types').insert({
+    id: fuel.id,
     fuel_type_code: fuel.fuel_type_code,
     fuel_type: fuel.fuel_type,
     count: fuel.count,
@@ -152,6 +168,7 @@ const createFuelType = (knex, fuel) => {
     fuel.stations.forEach( (station) => {
       fuelStationPromises.push(
         createFuelStation(knex, {
+          id: station.id,
           station_code: station.station_code,
           station_name: station.station_name,
           zip: station.zip,
--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -46,26 +46,63 @@ describe('API Routes', () => {
       chai.request(server)
       .get('/api/v1/fuels')
       .end((err, res) => {
-        const sortedBody = res.body.sort((a, b) => a.id - b.id);
-
+        res.body.sort((a, b) => a.id - b.id);
         res.should.have.status(200);
         res.should.be.json;
-        sortedBody[0].should.have.property('id');
-        sortedBody[0].should.have.property('fuel_type_code');
-        sortedBody[0].should.have.property('fuel_type');
-        sortedBody[0].should.have.property('count');
-        sortedBody[0].fuel_type_code.should.equal('E85');
-        sortedBody[0].fuel_type.should.equal('Ethanol (E85)');
-        sortedBody[0].count.should.equal(1);
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('fuel_type_code');
+        res.body[0].should.have.property('fuel_type');
+        res.body[0].should.have.property('count');
+        res.body[0].fuel_type_code.should.equal('E85');
+        res.body[0].fuel_type.should.equal('Ethanol (E85)');
+        res.body[0].count.should.equal(1);
         done();
       });
     });
 
-    it('should return a 404 if directed to a non existent endpoint', () => {
+    it('should return a 404 if directed to a non existent endpoint', done => {
       chai.request(server)
       .get('/api/v1/foobah')
       .end((err, res) => {
         res.should.have.status(404);
+        done()
+      });
+    });
+
+  });
+
+  describe('GET /api/v1/stations/fuels/:fuel_type_code', () => {
+    it('should return the list of stations associated with the fuel type code', done => {
+      chai.request(server)
+      .get('/api/v1/stations/fuels/ELEC')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.length.should.equal(2);
+        res.body.sort((a, b) => a.id - b.id);
+        res.body[0].id.should.equal(2);
+        res.body[0].zip.should.equal('04210');
+        res.body[0].latitude.should.equal('44.129233');
+        done();
+      });
+    });
+
+    it('should return a 404 error if there are no stations associated with the fuel code', done => {
+      chai.request(server)
+      .get('/api/v1/stations/fuels/HY')
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.error.should.equal('Could not locate stations with fuel_type_code HY');
+        done()
+      });
+    });
+
+    it('should return a 404 error if the fuel type code does not exist', done => {
+      chai.request(server)
+      .get('/api/v1/stations/fuels/caffeine')
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.error.should.equal('Could not locate fuel id with fuel_type_code caffeine');
         done()
       });
     });
@@ -77,29 +114,27 @@ describe('API Routes', () => {
       chai.request(server)
       .get('/api/v1/stations')
       .end((err, res) => {
-        const sortedBody = res.body.sort((a, b) => a.id - b.id);
-
-        sortedBody[0].should.have.property('id');
-        sortedBody[0].should.have.property('fuel_type_id');
-        sortedBody[0].should.have.property('station_code');
-        sortedBody[0].should.have.property('station_name');
-        sortedBody[0].should.have.property('zip');
-        sortedBody[0].should.have.property('state');
-        sortedBody[0].should.have.property('city');
-        sortedBody[0].should.have.property('street_address');
-        sortedBody[0].should.have.property('latitude');
-        sortedBody[0].should.have.property('longitude');
-        sortedBody[0].should.have.property('geocode_status');
-        sortedBody[0].station_code.should.equal(41733);
-        sortedBody[0].station_name.should.equal('Veterans Affairs - Maine Healthcare System - Togus');
-        sortedBody[0].zip.should.equal();
-        sortedBody[0].state.should.equal(41733);
-        sortedBody[0].city.should.equal(41733);
-        sortedBody[0].street_address.should.equal(41733);
-        sortedBody[0].latitude.should.equal(41733);
-        sortedBody[0].longitude.should.equal(41733);
-        sortedBody[0].geocode_status.should.equal(41733);
-        console.log(sortedBody);
+        res.body.sort((a, b) => a.id - b.id);
+        res.body[0].should.have.property('id');
+        res.body[0].should.have.property('fuel_type_id');
+        res.body[0].should.have.property('station_code');
+        res.body[0].should.have.property('station_name');
+        res.body[0].should.have.property('zip');
+        res.body[0].should.have.property('state');
+        res.body[0].should.have.property('city');
+        res.body[0].should.have.property('street_address');
+        res.body[0].should.have.property('latitude');
+        res.body[0].should.have.property('longitude');
+        res.body[0].should.have.property('geocode_status');
+        res.body[0].station_code.should.equal(41733);
+        res.body[0].station_name.should.equal('Veterans Affairs - Maine Healthcare System - Togus');
+        res.body[0].zip.should.equal('04330');
+        res.body[0].state.should.equal('ME');
+        res.body[0].city.should.equal('Augusta');
+        res.body[0].street_address.should.equal('1 VA Center');
+        res.body[0].latitude.should.equal('44.296869');
+        res.body[0].longitude.should.equal('-69.724097');
+        res.body[0].geocode_status.should.equal('GPS');
         res.should.have.status(200);
         res.should.be.json;
         done();
@@ -107,5 +142,180 @@ describe('API Routes', () => {
     });
   });
 
+  describe('GET /api/v1/stations/:station_code', () => {
+    it('should return the stations record asociated with the station code', done => {
+      chai.request(server)
+      .get('/api/v1/stations/63965')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.length.should.equal(1);
+        res.body[0].should.have.property('street_address');
+        res.body[0].should.have.property('latitude');
+        res.body[0].should.have.property('longitude');
+        res.body[0].street_address.should.equal('424 Main St');
+        res.body[0].latitude.should.equal('44.800429');
+        res.body[0].longitude.should.equal('-69.853698');
+        done();
+      });
+    });
+
+    it('should return a 404 error if the station code does not exist', done => {
+      chai.request(server)
+      .get('/api/v1/stations/100000')
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.error.should.equal('Could not locate station with id 100000')
+        done();
+      })
+    })
+
+  });
+
+  describe('POST /api/v1/stations', () => {
+    let reqBody = {
+      id: 333,
+      station_code: 12345,
+      station_name: "Ron's Cash Fuel Inc",
+      zip: '04950',
+      state: 'ME',
+      city: "Biddeford",
+      street_address: "34 Main St",
+      latitude: '54.800429',
+      longitude: '-67.853698',
+      geocode_status: "GPS",
+      fuel_type_code: 'CNG'
+    }
+
+    it('should create a new station', done => {
+      chai.request(server)
+      .post('/api/v1/stations')
+      .send(reqBody)
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.have.property('id');
+        res.body.id.should.equal(333)
+        done();
+      })
+    })
+
+    it('should not add a new station if missing a parameter', done => {
+      delete reqBody.station_code
+      chai.request(server)
+      .post('/api/v1/stations')
+      .send(reqBody)
+      .end((err, res) => {
+        res.should.have.status(422);
+        done();
+      });
+    });
+
+    it('should not add a new station if the fuel_type_code is invalid', done => {
+      let = reqBody2 = {
+        id: 333,
+        station_code: 12345,
+        station_name: "Ron's Cash Fuel Inc",
+        zip: '04950',
+        state: 'ME',
+        city: "Biddeford",
+        street_address: "34 Main St",
+        latitude: '54.800429',
+        longitude: '-67.853698',
+        geocode_status: "GPS",
+        fuel_type_code: 'ZZ'
+      }
+
+      chai.request(server)
+      .post('/api/v1/stations')
+      .send(reqBody2)
+      .end((err, res) => {
+        res.should.have.status(404)
+        res.body.error.should.equal('Could not locate fuel id with fuel_type_code ZZ');
+        done();
+      });
+    });
+
+  });
+
+  describe('POST /api/v1/fuels', () => {
+    it('should create a new fuel', done => {
+      chai.request(server)
+      .post('/api/v1/fuels')
+      .send({
+        id: 18,
+        fuel_type_code: 'LM',
+        count: 0,
+        fuel_type: 'liquid magma'
+      })
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.have.property('id');
+        res.body.id.should.equal(18);
+        done();
+      });
+    });
+
+    it('should not add a new fuel if missing a parameter', done => {
+      chai.request(server)
+      .post('/api/v1/fuels')
+      .send({
+        id: 18,
+        fuel_type_code: 'LM',
+        count: 0
+      })
+      .end((err, res) => {
+        res.should.have.status(422);
+        res.body.error.should.equal('Expected format: { fuel_type_code: <string>, fuel_type: <string>, count: <integer> } You are missing a fuel_type property.')
+        done();
+      });
+    });
+  });
+
+  describe('DELETE /api/v1/stations/station_code', () => {
+    it('should delete the station', done => {
+      chai.request(server)
+      .delete('/api/v1/stations/48463')
+      .end((err, res) => {
+        res.should.have.status(204);
+        done();
+      });
+    });
+
+    it('should not delete the station if the station code is invalid', done => {
+      chai.request(server)
+      .delete('/api/v1/stations/10000000')
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.body.error.should.equal('Could not locate station with code 10000000');
+        done();
+      });
+    });
+  });
+
+  describe('PATCH /api/v1/fuels/:fuel_type_code/:count', () => {
+    it('should patch the station count to the fuel specified', done => {
+      chai.request(server)
+      .patch('/api/v1/fuels/HY/2')
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.response.should.equal('Count successfully updated for HY');
+        done();
+      });
+    });
+
+    // it('should not patch the station count to the fuel if the fuel_type_code is invalid', done => {
+    //   chai.request(server)
+    //   .patch('/api/v1/fuels/HY/2')
+    //   .end((err, res) => {
+    //     res.should.have.status(201);
+    //     console.log(res.body);
+    //     // res.body..status()
+    //     done();
+    //   });
+    // });
+
+  });
 
 });
